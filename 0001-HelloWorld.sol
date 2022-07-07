4ff83b42b18bd1a06a4515d1bab2c90249c4da7f
thejackforge <kimfidler@gmail.com>
2022-07-06 22:01:49 -0600

    First commit - 0001 and 0002

---
+++ 0001-HelloWorld.sol
@@ -0,0 +1,9 @@
+// pragma specifies the compiler version of Solidity
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract HelloWorld {
+    string public greet = "Hello World";
+}
