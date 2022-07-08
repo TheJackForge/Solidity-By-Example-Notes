cdd9cca384a4a52c79cf47de2da7a384569fb81d
thejackforge <kimfidler@gmail.com>
2022-07-07 21:55:00 -0600

    Primitive Data Types Complete

--- 0003-Primitive-Data-Types.sol
+++ 0003-Primitive-Data-Types.sol
@@ -23,10 +23,35 @@ contract Primitives {
         uint256: ranges from 0 to 2 ** 256 - 1
     */
 
-    uint8 public i8 = 1;
+    uint8 public u8 = 1;
     uint public u256 = 456;
     uint public u = 123;  // uint is an alias for 256
 
-    
+    // minimum and maximum of int
+
+    int public minInt = type(int).min;
+    int public maxInt = type(int).max;
+
+    address public addr = 0x19D88eAcB849dBF4B679c4788847f10cAb3A3E69;
+
+    /* In Solidity, the data type byte represents a sequence of bytes.
+    Solidity presents two types of byte types.
+
+    - Fixed-size byte arrays
+    - Dynamically-sized byte arrays
+
+    The term bytes in Solidity represents a dynamic array of bytes.
+    It's a shorthand for byte[] */
+
+    bytes1 a = 0xb5; // [10110101]
+    bytes1 b = 0x56; // [01010110]
+
+    // Default Values
+    // Unassigned Variables have a default value
+
+    bool public defaultBoo; // False
+    uint public defaultUint; // 0
+    int public defaultInt; // 1
+    address public defaultAddr; // 0x0000000000000000000000000000000000000000
 
 }
